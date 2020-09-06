e8a5395e504ad3c3c363372b5f628658783bbd89
karanbm23 <karanbm23@gmail.com>
2020-09-06 20:48:07 +0530

    Team: CODE THUGS (KARAN B M) AWS IP Address: http://18.191.230.82:8080/PHR/
    1) Patient SMS OTP fixed.
    2) Replaced "Final year Project work" with "XON LABS"
    3) Fixed Mobile number input field to accept only numericals(10 digits).

--- PHR/src/com/phr/mobile/SendMessage.java
+++ PHR/src/com/phr/mobile/SendMessage.java
@@ -4,46 +4,35 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-public class SendMessage
-{
-   public static String SMS_GATEWAY_API_KEY = "8iCEUKf4no8-QTFFscIIcpJ7lKP7OzGjW8Cxvdzg7n";
-//   public static String SMS_GATEWAY_API_KEY = "8iC";
-
-   public static String sendSms(String number, String msg)
-   {
-      try
-      {
-         // Construct data
-         String apiKey = "apikey=" + SMS_GATEWAY_API_KEY;
-         String message = "&message=" + msg;
-         String sender = "&sender=" + "TXTLCL";
-         String numbers = "&numbers=" + number;
-
-         // Send data
-         HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
-         String data = apiKey + numbers + message + sender;
-         conn.setDoOutput(true);
-         conn.setRequestMethod("POST");
-         conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-         conn.getOutputStream().write(data.getBytes("UTF-8"));
-         final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-         final StringBuffer stringBuffer = new StringBuffer();
-         String line;
-         while ((line = rd.readLine()) != null)
-         {
-            stringBuffer.append(line);
-         }
-         rd.close();
-
-         System.out.println("SMS Result: " + stringBuffer.toString());
-         return stringBuffer.toString();
-      }
-      catch (Exception e)
-      {
-         System.out.println("Error SMS " + e);
-         return "Error " + e;
-      }
-   }
-
+public class SendMessage {
+	
+	public static String sendSms(String number, String msg) {
+		try {
+			// Construct data
+			// Send data
+			String apiKey = "NEg6SGKj78Fd2APfDV4rBwsMQpJvYkXxblLhzaeHoRty5UWIcOtK40A1vefNDP2kOjgsbWn8XyorlMQ6";
+			HttpURLConnection conn = (HttpURLConnection) new URL("https://www.fast2sms.com/dev/bulk").openConnection();
+			String data = "sender_id=FSTSMS&message=" + msg + "&language=english&route=p&numbers=" + number;
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("authorization", apiKey);
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+			conn.setRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36");
+			conn.getOutputStream().write(data.getBytes("UTF-8"));
+			final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			final StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = rd.readLine()) != null) {
+				stringBuffer.append(line);
+			}
+			rd.close();
+			System.out.println("SMS Result: " + stringBuffer.toString());
+			return stringBuffer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Error " + e;
+		}
+	}
 }
